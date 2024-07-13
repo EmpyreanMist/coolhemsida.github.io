3fa9957e730978924f8de87a1d66d210a848db56
EmpyreanMist <christianfryksten1@gmail.com>
2024-07-13 10:17:10 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,12 @@
+//console.log('Hello'); 
+
+// window.alert('Warning! Warning! \nThis website might be too cool for you!');
+
+
+function addition (){
+  asdo +asdo
+}
+
+//console.log("Tjenare");
+
+
